88ec69daa1eda634f2951bd2c5743e85d2cacf00
nluu <mrnick731@gmail.com>
2024-10-07 16:38:48 -0700

    added questions with stubs and tests

--- src/test/model/TestSample.java
+++ src/test/model/TestSample.java
@@ -5,15 +5,38 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestSample {
+public class NotesTest {
     
     @BeforeEach
     void runBefore() {
-
+        testNotes = new Note("CPSC210");
     }
 
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void testConstructor() {
+        assertEqual("CPSC210", testNotes.getCourse());
+        assertEqual(0, testNotes.getNumQuestions());
+    }
+
+    @Test
+    void testAddOneQuestionAnswer() {
+        assertEqual(0, testNotes.getNumQuestions());
+        testNotes.addQA("What is an integer?", "A Whole Number");
+        assertEqual(1, testNotes.getNumQuestions());
+        assertEqual("What is an integer?", testNotes.getQuestion(0));
+        assertEqual8("A Whole Number", testNotes.getAnswer(0));
+    }
+    @Test
+    testList = new ArrayList<>;
+    testList.add("What is an integer?");
+    testList.add("What is a string?");
+
+    void testAddMultipleQuestionAnswer() {
+        assertEqual(0, testNotes.getNumQuestions());
+        testNotes.addQA("What is an integer?", "A Whole Number");
+        testNotes.addQA("What is a string?", "A combination of characters");
+        assertEqual(2, testNotes.getNumQuestions());
+        assertEqual("What is an integer?", testNotes.getQuestion(0));
+        assertEqual("A Whole Number", testNotes.getAllQuestions());
     }
 }
