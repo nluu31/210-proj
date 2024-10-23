e733a37f74da53ac4e2a5bb5aa03130ea3bba5eb
nluu <mrnick731@gmail.com>
2024-10-23 15:51:36 -0700

    modified persistence tests

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // Referenced from the JsonSerialization Demo
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+
 public class JsonTest {
     protected void checkQA(String question, String answer, String unit, QuestionAnswer qa) {
         assertEquals(question, qa.getQuestion());
